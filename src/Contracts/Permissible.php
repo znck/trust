76d6123392577a6bcf853696f52cdfbd9b8ae4e9
Rahul Kadyan <hi@znck.me>
2017-01-17 15:32:56 +0530

    :memo: Format doc comments

--- src/Contracts/Permissible.php
+++ src/Contracts/Permissible.php
@@ -6,7 +6,6 @@ interface Permissible
      * Checks if the user has Permission.
      *
      * @param  string|Permission $permission
-     *
      * @return bool
      */
     public function hasPermissionTo($permission);
@@ -15,7 +14,6 @@ interface Permissible
      * Check if the user has Role.
      *
      * @param  string|Role $role
-     *
      * @return bool
      */
     public function canAssumeRole($role);
